64f822a443b388a0e5fc78963a4852ebaf1104b6
KadeDevelopers <84626220+KadeDevelopers@users.noreply.github.com>
2021-08-21 14:04:58 -0400

    HAZEL UPDATE

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -25,7 +25,8 @@ class MenuCharacter extends FlxSprite
 		'bf' => new CharacterSetting(0, -20, 1.0, true),
 		'gf' => new CharacterSetting(50, 80, 1.5, true),
 		'dad' => new CharacterSetting(-15, 130),
-		'clip' => new CharacterSetting(-15, 130, 1, true)
+		'clip' => new CharacterSetting( -15, 130, 1, true),
+		'hazel' => new CharacterSetting(-15, 110, 1)
 	];
 
 	private var flipped:Bool = false;
@@ -42,8 +43,9 @@ class MenuCharacter extends FlxSprite
 		animation.addByPrefix('bf', "BF idle dance white", 24);
 		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
 		animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
-		animation.addByPrefix('dad', "Clip Idle Dance copy 2", 24);
-		animation.addByPrefix('clip', "Clip Idle Dance copy 2", 24);
+		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
+		animation.addByPrefix('clip', "Clip Idle Dance copy", 24);
+		animation.addByPrefix('hazel', "hazel_idle_dance", 24);
 
 		setGraphicSize(Std.int(width * scale));
 		updateHitbox();
