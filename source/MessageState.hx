d9ff70f3048af0dc879b18a62b1fe795b1e91ea0
AydenDaCringe <84626220+KadeDevelopers@users.noreply.github.com>
2021-08-31 07:27:11 -0400

    fixes and bits

--- source/MessageState.hx
+++ source/MessageState.hx
@@ -6,38 +6,37 @@ import flixel.text.FlxText;
 
 class MessageState extends MusicBeatState
 {
-
     override function create()
     {
         super.create();
 
-        if (FlxG.random.bool(0.1))
-		{
-			// something
-		}
-
-        FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
-        FlxG.sound.music.fadeIn(4, 0, 0.7);
-
+        // this line of code is broken for now. gonna try to find a way to fix it
         if (FlxG.keys.justPressed.ENTER)
-        {
             FlxG.switchState(new MainMenuState());
 
-            trace("LEAVING THE STATION");
+		if (!FlxG.sound.music.playing)
+        {
+            FlxG.sound.playMusic(Paths.music('freakyMenu'));
         }
 
-        var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
-		bg.scrollFactor.x = 0;
+        // var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+		// bg.scrollFactor.x = 0;
+		// bg.scrollFactor.y = 0.15;
+		// bg.setGraphicSize(Std.int(bg.width * 1.1));
+		// bg.updateHitbox();
+		// bg.screenCenter();
+		// bg.antialiasing = true;
+		// add(bg);
+
+        var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('city/sunset_port', 'shared'));
+        bg.screenCenter();
+        bg.scrollFactor.x = 0;
 		bg.scrollFactor.y = 0.15;
-		bg.setGraphicSize(Std.int(bg.width * 1.1));
-		bg.updateHitbox();
-		bg.screenCenter();
-		bg.antialiasing = true;
-		add(bg);
+        add(bg);
 
         var messageText:FlxText = new FlxText(100, 100, FlxG.width);
         messageText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
-        messageText.text = "HEY DUMBASS, TO ATTACK AND DODGE, PRESS THE SPACE BAR 3 TIMES IN A SECOND.\nIF YOU READ THE GAMEBANANA DESCRIPTION, YOU'D KNOW THIS... -CLIP";
+        messageText.text = "HEY DUMBASS, TO ATTACK AND DODGE, PRESS THE SPACE BAR 3 TIMES IN A SECOND.\nIF YOU READ THE GAMEBANANA DESCRIPTION, YOU'D KNOW THIS... -CLIP\nPRESS ENTER TO PROCEED";
         messageText.screenCenter();
         messageText.size = 30;
         add(messageText);
